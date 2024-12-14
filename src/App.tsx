3a7224d0310ae6aebc1beb0d3bb09ee365b45c46
Zak Hynd <zakhynd@gmail.com>
2024-12-14 18:57:55 +1300

    reopoint github repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
 
-// const MainPage = lazy(() =>
-// 	import('./pages/Main/Main.page').then((module) => ({
-// 		default: module.MainPage
-// 	}))
-// );
-
-// export const App: React.FC = () => {
-// 	return (
-// 		<div className='bg-bg w-screen h-screen relative'>
-// 			<Suspense fallback={<Loader />}>
-// 				<Routes>
-// 					<Route path='/' element={<MainPage />} />
-// 					<Route path='settings' element={<SettingsPage />} />
-// 				</Routes>
-// 			</Suspense>
-// 		</div>
-// 	);
-// };
-
 export const App: React.FC = () => {
-	return <div>App</div>;
-};
+	return (
+	<div>
+		This is a homepage
+	</div>
+
+)}
