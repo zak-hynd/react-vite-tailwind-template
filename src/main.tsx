ad6ef03ffa5c3d1140cdcaf18bf49bdc9d3b7eca
Zak Hynd <zakhynd@gmail.com>
2024-12-14 21:38:02 +1300

    switch to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { App } from './App';
 
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<BrowserRouter basename='/react-vite-tailwind-template'>
+	<HashRouter>
 		<App />
-	</BrowserRouter>
-);
+	</HashRouter>
+)
