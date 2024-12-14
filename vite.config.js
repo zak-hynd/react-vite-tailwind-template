134fae1103447430511a8f9bdc687f33321f276e
Zak Hynd <zakhynd@gmail.com>
2024-12-14 19:22:07 +1300

    change base to root

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/react-vite-tailwind-template/',
+	base: '/',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
