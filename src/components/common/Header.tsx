1e88e9632174ad138e653e732c690d80ecaf1851
Zak Hynd <zakhynd@gmail.com>
2024-12-16 11:43:30 +1300

    rewrangle links into nav bar

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,10 +2,46 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-      <header className="text-left p-2">
-        <Link to="/" className="font-bold text-lg">
-          {`{zh}`}
-        </Link>
-      </header>
+    <header className="p-2 flex justify-between items-center">
+      <Link to="/" className="font-bold text-lg">
+        {`{zh}`}
+      </Link>
+      <div className="flex space-x-4 text-lg">
+        <a
+          href="https://www.linkedin.com/in/zak-hynd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a
+          href="https://github.com/zak-hynd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github"></i>
+        </a>
+      </div>
+    </header>
+      // <header className="p-2">
+
+      //   <Link to="/" className="font-bold text-lg text-left">
+      //     {`{zh}`}
+      //   </Link>
+
+      //   <div className="space-x-6">	
+      //     <a href="https://www.linkedin.com/in/zak-hynd" target="_blank" 
+      //       rel="noopener noreferrer" className="flex items-center">
+      //       <i className="fab fa-linkedin"></i>
+      //     </a>
+
+      //     <a 
+      //       href="https://github.com/zak-hynd" target="_blank" 
+      //       rel="noopener noreferrer" className="flex items-center">
+      //       <i className="fab fa-github"></i>
+      //     </a>
+      //   </div>
+        
+      // </header>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import Title from "@/components/common/Title"
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     <div>
       <Title />
+      <div className="text-lg pb-4">
+        <Link to="/about">{`about`}</Link>
+      </div>
+        
       <h2 className="font-bold">
         Some projects...
       </h2>
