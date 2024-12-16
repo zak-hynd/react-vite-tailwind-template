a7cba3f94d77d958960b7e37f97bd2962eb17f1e
Zak Hynd <zakhynd@gmail.com>
2024-12-16 20:16:22 +1300

    describe other projects

--- src/components/Porjects.tsx
+++ src/components/Porjects.tsx
@@ -72,6 +72,21 @@ const Porjects = () => {
           </p>
           <img src="/media/KesKeSayTests.webp" alt="Kes Ke Say test coverage, highlighting 100% coverage of user/profile code" style={{ maxWidth: "60%", height: "auto" }} />
         </div>
+
+        <div className="panel my-2">
+          <h3>Other projects</h3>
+          <p>
+            The "personal project" I started months ago hasn't had much love in a while.
+            It was started when I was still working with simple JavaScript and HTML pages, but once we started learning TypeScript and React, I found that a lot of my previous work was obsolete!
+            My intention is to be able to upload, parse, and analyse electricity usage data (csv files), initially just from Electric Kiwi, and compare the usage costs accross a range of Electricity pricing plans.
+            Now that I have a better understanding of how to structure and write code, and have a bit more time on my hands, I ought to be able to make more progress on this.
+          </p>
+          <p>
+            Comprehensive weather alerts. A good friend of mine, Shub, and I plan to build an old idea of mine.
+            One of the problems that we're intending to solve is that when you want to do an outdoor activity, you often have to consult multiple sources in order to figure out if it's going to be suitable. Surfing, Paragliding, Kitesurfing, Rock climbing, Mountain Biking, Skiing, Garden watering, Stargazing, Aurora Watching...
+            Perhaps we can monetise it... Perhaps we <i>have to</i> monetise it to cover the costs of all the API calls! Things to discover...
+          </p>
+        </div>
       </section>
     );
   };
